d8cba254b869f2e1b0aa27f554af94d1924b682e
stanmqz <moscarstanley37@gmail.com>
2026-06-13 15:43:49 -0600

    feat: activate lesson 3 button and add navigation functionality

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,12 @@ lesson6Btn.addEventListener("click", () => {
     window.location.href = "/section6.html";
 });
 
+const lesson3Btn = document.getElementById("lesson-3-btn");
+
+lesson3Btn.addEventListener("click", () => {
+    window.location.href = "/section3.html";
+});
+
 const lesson5Btn = document.getElementById("lesson-5-btn");
 
 lesson5Btn.addEventListener("click", () => {
