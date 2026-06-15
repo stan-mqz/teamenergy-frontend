1262e34e7f1834f925a67f8b9bb10e859af239aa
Anderson Ortiz <ortizone620@gmail.com>
2026-06-15 00:15:12 -0600

    feat: update loadMechanicsPage to use createStats1 and improve lesson navigation integration

--- src/js/pages/section1.page.js
+++ src/js/pages/section1.page.js
@@ -1,10 +1,12 @@
 import { createQuiz } from '../components/section1.quiz.js';
 import { createStats1 } from '../components/section1.stats.js';
+import { createLessonNavigation } from '../components/lesson.navigation.js';
+import '../../css/lesson.navigation.css';
 
 export async function loadMechanicsPage() {
     try {
         await createQuiz();
-        await createStats();
+        await createStats1();
         app.appendChild(createLessonNavigation(1));
     } catch (err) {
         console.error("Error cargando la sección 1:", err);
